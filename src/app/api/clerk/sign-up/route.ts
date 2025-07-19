52a6744c198b5c7d7712baf904d0e317fd9bef8e
Musheer0 <147691647+Musheer0@users.noreply.github.com>
2025-07-19 14:37:19 +0530

    setup webhooks

--- src/app/api/clerk/sign-up/route.ts
+++ src/app/api/clerk/sign-up/route.ts
@@ -1,5 +1,31 @@
-import { NextRequest } from "next/server";
+import prisma from '@/db'
+import { verifyWebhook } from '@clerk/nextjs/webhooks'
+import { NextRequest, NextResponse } from 'next/server'
 
-export const POST = async(req:NextRequest)=>{
-    
+export async function POST(req: NextRequest) {
+  try {
+    const evt = await verifyWebhook(req)
+    const { id } = evt.data
+    const eventType = evt.type
+    try {
+        if(!id || eventType!=='user.created') return NextResponse.json({error:'clerk id not found'},{status:500})
+            const {data} = evt;
+        
+     await prisma.user.create({
+            data:{
+                clerk_id: id,
+                email: data.email_addresses.map((e)=>e.email_address),
+                name:`${data.first_name} ${data.last_name}`
+            }
+        });
+    } catch (error) {
+        console.log(error)
+        return NextResponse.json({error:'error creating user'},{status:500})
+    }
+
+    return new Response('Webhook received', { status: 200 })
+  } catch (err) {
+    console.error('Error verifying webhook:', err)
+    return new Response('Error verifying webhook', { status: 400 })
+  }
 }
--- src/app/api/clerk/user-edit/route.ts
+++ src/app/api/clerk/user-edit/route.ts
@@ -1,5 +1,33 @@
-import { NextRequest } from "next/server";
+import prisma from '@/db'
+import { verifyWebhook } from '@clerk/nextjs/webhooks'
+import { NextRequest } from 'next/server'
 
-export const POST = async(req:NextRequest)=>{
-    
+export async function POST(req: NextRequest) {
+  try {
+    const evt = await verifyWebhook(req,{signingSecret:process.env.CLERK_WEBHOOK_USEREDITING_SECRET});
+    const { id } = evt.data
+    const eventType = evt.type
+    console.log(`Received webhook with ID ${id} and event type of ${eventType}`)
+    console.log(' Webhook payload:', evt.data)
+    if(!id || eventType!=='user.updated')     return new Response('Error verifying webhook', { status: 400 });
+    try {
+        await prisma.user.update({
+            where:{
+                clerk_id:id,
+            },
+            data:{
+                email:evt.data.email_addresses.map((e)=>e.email_address),
+                name:`${evt.data.first_name} ${evt.data.last_name}`
+            }
+        })
+    } catch (error) {
+        console.log(error);
+            return new Response('Error updating user', { status: 400 })
+
+    }
+    return new Response('Webhook received', { status: 200 })
+  } catch (err) {
+    console.error('Error verifying webhook:', err)
+    return new Response('Error verifying webhook', { status: 400 })
+  }
 }
