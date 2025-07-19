45ff9c25f1ce7056902f617dea6ada1a3ac4a3a1
Musheer0 <147691647+Musheer0@users.noreply.github.com>
2025-07-19 20:43:58 +0530

    setup sandbox & inngest

---
+++ src/actions/get-sandbox.ts
@@ -0,0 +1,10 @@
+import { Sandbox } from '@e2b/code-interpreter'
+
+export const GetSandBoxId = async()=>{
+    const sandbox = await Sandbox.create(process.env.E2B_TEMPLATE!);
+    return sandbox.sandboxId;
+}
+
+export const GetSandBox = async(id:string)=>{
+    return await Sandbox.connect(id);
+}
