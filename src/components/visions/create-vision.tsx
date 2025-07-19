45bb232c3541c0347d483ff35d9c57e0af3d737c
Musheer0 <147691647+Musheer0@users.noreply.github.com>
2025-07-20 02:15:27 +0530

    setup create vision

--- src/components/visions/create-vision.tsx
+++ src/components/visions/create-vision.tsx
@@ -1,9 +1,36 @@
 "use client"
-import React from 'react'
-
+import { useTrpc } from '@/trpc/client';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import React, { useState } from 'react'
+import TextareaAutosize from 'react-textarea-autosize';
+import { Button } from '../ui/button';
+import { ArrowUp, Loader2 } from 'lucide-react';
+import {toast} from 'sonner'
 const CreateVision = () => {
+    const [prompt,setPrompt] = useState('');
+    const trpc = useTrpc();
+    const query_client = useQueryClient()
+    const { mutate, isError ,isPending} =useMutation({
+        mutationFn:async()=>await trpc.vision.create.mutate({prompt}),
+        onSuccess:(data)=>{
+            if(data?.warning)toast.warning(data.warning)
+            query_client.setQueryData([data.vision.id],{...data.vision, fragments:data.fragments})
+        }
+    });
   return (
-    <div>CreateVision</div>
+    <section className='w-full flex-1 flex flex-col items-center justify-center'>
+        {isError && <p>Error </p>}
+      <TextareaAutosize
+      value={prompt}
+      onChange={(e)=>setPrompt(e.target.value)}
+      minRows={3}
+      maxRows={10}
+      disabled={isPending}
+      />
+      <Button size={'icon'} onClick={()=>mutate()} disabled={isPending}>
+        {isPending ? <Loader2 className='animate-spin'/>:<ArrowUp/>}
+      </Button>
+    </section>
   )
 }
 
